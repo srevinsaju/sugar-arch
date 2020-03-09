79fa4be5ef113a8592e1ce332ce6f34e9913db40
shaansubbaiah <shaansubbaiah.cs18@bmsce.ac.in>
2020-03-09 20:26:53 +0530

    stable autoinstall scipt fixed

--- stable/autoinstall.sh
+++ stable/autoinstall.sh
@@ -1,5 +1,8 @@
-for $module in sugar{-artwork, -toolkit-gtk3, -datastore, ,-runner, -toolkit2-gtk3}; do
+#!/bin/sh
+for module in sugar-artwork sugar-toolkit-gtk3 sugar-datastore sugar sugar-runner sugar-toolkit2-gtk3 gwebsockets;
+do
 	cd $module
 	makepkg -si 
 	cd ..
+done
 echo Done
